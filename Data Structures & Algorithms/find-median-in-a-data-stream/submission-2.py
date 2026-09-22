bec5844c87442fd78a2b60d62ffd1f432ea3a5e2
Raphaël Verger <109467967+raphael-verger@users.noreply.github.com>
2026-09-22 19:27:59 +0100

    Add: find-median-in-a-data-stream - submission-2

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-2.py
@@ -0,0 +1,24 @@
+import bisect
+
+class MedianFinder:
+
+    def __init__(self):
+        self.store = []
+
+    def addNum(self, num: int) -> None:
+        bisect.insort(self.store, num)
+
+    def findMedian(self) -> float:
+        n = len(self.store)
+
+        if n==0:
+            return None
+        if n==1:
+            return float(self.store[0])
+
+        mid = n//2 
+
+        if n%2==1:
+            return float(self.store[mid])
+        else:
+            return float((self.store[mid-1])+(self.store[mid]))/2
